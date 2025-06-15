4bf246556b939ce5a58fcc586ff06621ddd2cf20
bohconcepts <sefa@bohconcepts.com>
2025-06-15 14:08:24 -0400

    Remove Open AI chat information

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import DocumentChat from '../ui/document-chat';
 
 interface LayoutProps {
   children: ReactNode;
@@ -15,7 +14,6 @@ const Layout = ({ children }: LayoutProps) => {
         {children}
       </main>
       <Footer />
-      <DocumentChat />
     </div>
   );
 };
