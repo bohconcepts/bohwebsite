02721aa4bb7864b8411052d0a28c3b390fffa5cb
bohconcepts <sefa@bohconcepts.com>
2025-06-18 19:18:23 -0400

    Khalid Requested Changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
+import './styles/fonts.css';
+import './styles/typography.css'; // Import typography adjustments
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
